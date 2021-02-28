7cba0c47f3227ad13fea4f0836958952b6f1a7ef
mbhoxsie <matt.hoxsie@gmail.com>
2021-02-27 21:38:58 -0500

    Added default commands for SS's in Robot Container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -14,18 +14,26 @@ import frc.robot.subsystems.DriveTrainSubsystem;
 //import edu.wpi.first.wpilibj2.command.button.Button;
 import frc.robot.subsystems.ElevatorSubsystem;
 import frc.robot.StaticConstants.XBoxJoystickConstants;
+import frc.robot.commands.BlingCommands.BlingSetDefaultCommand;
+import frc.robot.commands.DriveTrainCommands.DriveWithJoystickCommand;
 import frc.robot.commands.ElevatorCommands.GoDownCommand;
 import frc.robot.commands.ElevatorCommands.GoUpCommand;
+import frc.robot.commands.ElevatorCommands.StopElevatorCommand;
 import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.commands.ShooterCommands.ShooterOffCommand;
 import frc.robot.commands.ShooterCommands.ShooterOnCommand;
 import frc.robot.subsystems.TurretSubsystem;
 import frc.robot.commands.TurretCommands.TurretMotorCounterClockwiseCommand;
+import frc.robot.commands.TurretCommands.StopTurretCommand;
 import frc.robot.commands.TurretCommands.TurretMotorClockwiseCommand;
 import frc.robot.subsystems.HopperSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.LimelightSubsystem;
 import frc.robot.commands.HopperCommands.HopperGoBackwardCommand;
 import frc.robot.commands.HopperCommands.HopperGoForwardCommand;
+import frc.robot.commands.HopperCommands.StopHopperCommand;
+import frc.robot.commands.IntakeCommands.IntakeStopCommand;
+import frc.robot.commands.LimelightCommands.LimelightInitForVisionCommand;
 
 
 
@@ -39,32 +47,58 @@ public class RobotContainer {
    // The robot's subsystems and commands are defined here...
   
   //  Subsystems
-  private final ShooterSubsystem shooter = new ShooterSubsystem();
-  private final TurretSubsystem turret = new TurretSubsystem();
-  private final ElevatorSubsystem ellivator = new ElevatorSubsystem();
-  private final HopperSubsystem hopper = new HopperSubsystem();
-  private final IntakeSubsystem intake = new IntakeSubsystem();
-  private final BlingSubsystem bling = new BlingSubsystem();
-  private final DriveTrainSubsystem driveTrain = new DriveTrainSubsystem();
+  private final ShooterSubsystem m_shooterSubsystem = new ShooterSubsystem();
+  private final TurretSubsystem m_turretSubsystem = new TurretSubsystem();
+  private final ElevatorSubsystem m_elevatorSubsystem = new ElevatorSubsystem();
+  private final HopperSubsystem m_hopperSubsystem = new HopperSubsystem();
+  private final IntakeSubsystem m_intakeSubsystem = new IntakeSubsystem();
+  private final BlingSubsystem m_blingSubsystem = new BlingSubsystem();
+  private final DriveTrainSubsystem m_driveTrainSubsystem = new DriveTrainSubsystem();
+  private final LimelightSubsystem m_limelightSubsystem = new LimelightSubsystem();
+
+//  Joysticks
+  private final XboxController m_driverStick = new XboxController(XBoxJoystickConstants.DRIVERSTICK_USB_PORT);
+  private final XboxController m_operStick = new XboxController(XBoxJoystickConstants.OPERSTICK_USB_PORT); 
 
   //    Commands
-  private final ShooterOnCommand shooterOn = new ShooterOnCommand(shooter);
-  private final ShooterOffCommand shooterOff = new ShooterOffCommand(shooter);
-  private final TurretMotorCounterClockwiseCommand turretMotorLeftCommand = new TurretMotorCounterClockwiseCommand(turret);
-  private final TurretMotorClockwiseCommand turretMotorRightCommand = new TurretMotorClockwiseCommand(turret); 
-  private final HopperGoForwardCommand hopperGoForward = new HopperGoForwardCommand(hopper);
-  private final HopperGoBackwardCommand hopperGoBackward = new HopperGoBackwardCommand(hopper);
-  private final GoDownCommand goback = new GoDownCommand(ellivator);
-  private final GoUpCommand ellirun = new GoUpCommand(ellivator);
-
-  //  Joysticks
-  private final XboxController driverStick = new XboxController(XBoxJoystickConstants.DRIVERSTICK_USB_PORT);
-  private final XboxController operStick = new XboxController(XBoxJoystickConstants.OPERSTICK_USB_PORT); 
-  
-   
+  private final ShooterOnCommand shooterOnCommand = new ShooterOnCommand(m_shooterSubsystem);
+  private final ShooterOffCommand shooterOffCommand = new ShooterOffCommand(m_shooterSubsystem);
+  private final TurretMotorCounterClockwiseCommand turretMotorLeftCommand = new TurretMotorCounterClockwiseCommand(m_turretSubsystem);
+  private final TurretMotorClockwiseCommand turretMotorRightCommand = new TurretMotorClockwiseCommand(m_turretSubsystem); 
+  private final StopTurretCommand turretStopCommand = new StopTurretCommand(m_turretSubsystem);
+  private final HopperGoForwardCommand hopperGoForward = new HopperGoForwardCommand(m_hopperSubsystem);
+  private final HopperGoBackwardCommand hopperGoBackward = new HopperGoBackwardCommand(m_hopperSubsystem);
+  private final StopHopperCommand hopperStopCommand = new StopHopperCommand(m_hopperSubsystem);
+  private final GoDownCommand elevatorGoDownCommand = new GoDownCommand(m_elevatorSubsystem);
+  private final GoUpCommand elevatorGoUpCommand = new GoUpCommand(m_elevatorSubsystem);
+  private final StopElevatorCommand elevatorStopCommand = new StopElevatorCommand(m_elevatorSubsystem);
+  private final DriveWithJoystickCommand driveWithJoystickCommand = new DriveWithJoystickCommand(m_driveTrainSubsystem, m_driverStick);
+  private final LimelightInitForVisionCommand limelightInitForVisionCommand = new LimelightInitForVisionCommand(m_limelightSubsystem);
+  private final BlingSetDefaultCommand blingSetDefaultCommand = new BlingSetDefaultCommand(m_blingSubsystem);
+  private final IntakeStopCommand intakeStopCommand = new IntakeStopCommand(m_intakeSubsystem); 
   
     /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+
+    //  DEFAULT COMMANDS FOR EACH SUBSYSTEM
+    //This is where we make the DriveWithJoystick Command the default command for the DriveTrain
+    //  Drivetrain Default:  Drive with Joystick   
+    m_driveTrainSubsystem.setDefaultCommand(driveWithJoystickCommand);
+    //  Limelight Default:  Limelight Init for Vision
+    m_limelightSubsystem.setDefaultCommand(limelightInitForVisionCommand);
+    //  Bling Default:  Bling with the Default Pattern
+    m_blingSubsystem.setDefaultCommand(blingSetDefaultCommand);
+    //  Shooter Default:  Shooter Off
+    m_shooterSubsystem.setDefaultCommand(shooterOffCommand);
+    //  Elevator Default:  Elevator Off
+    m_elevatorSubsystem.setDefaultCommand(elevatorStopCommand);
+    //  Hopper Default:  Hopper Off
+    m_hopperSubsystem.setDefaultCommand(hopperStopCommand);
+    //  Turret Default:  Turret Off
+    m_turretSubsystem.setDefaultCommand(turretStopCommand);
+    //  Intake Default:  Intake Off
+    m_intakeSubsystem.setDefaultCommand(intakeStopCommand);
+
 
     // Configure the button bindings
     configureButtonBindings();
@@ -77,14 +111,14 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
-    new JoystickButton(driverStick, 1).whenPressed(shooterOn);
-    new JoystickButton(driverStick, 2).whenPressed(shooterOff);
-    new JoystickButton(driverStick, 3).whileHeld(turretMotorRightCommand);
-    new JoystickButton(driverStick, 4).whileHeld(turretMotorLeftCommand);
-    new JoystickButton(driverStick, Button.kB.value).whileHeld(ellirun);
-    new JoystickButton(driverStick, Button.kX.value).whileHeld(goback);
-    new JoystickButton(driverStick, Button.kBumperLeft.value).whileHeld(hopperGoForward);
-    new JoystickButton(driverStick, Button.kBumperRight.value).whileHeld(hopperGoBackward);
+    new JoystickButton(m_driverStick, Button.kA.value).whenPressed(shooterOnCommand);
+    new JoystickButton(m_driverStick, Button.kY.value).whenPressed(shooterOffCommand);
+    new JoystickButton(m_driverStick, Button.kBumperLeft.value).whileHeld(turretMotorRightCommand);
+    new JoystickButton(m_driverStick, Button.kBumperRight.value).whileHeld(turretMotorLeftCommand);
+    new JoystickButton(m_driverStick, Button.kB.value).whileHeld(elevatorGoUpCommand);
+    new JoystickButton(m_driverStick, Button.kX.value).whileHeld(elevatorGoDownCommand);
+    new JoystickButton(m_driverStick, Button.kBack.value).whileHeld(hopperGoForward);
+    new JoystickButton(m_driverStick, Button.kStart.value).whileHeld(hopperGoBackward);
   }
 
   /**
