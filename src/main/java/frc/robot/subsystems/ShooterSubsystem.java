0907361733b3e1d6a6be267a2a939f698dd725f4
mbhoxsie <matt.hoxsie@gmail.com>
2021-03-14 11:00:53 -0400

    Added a method to pass a constant shooter velocity

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -15,36 +15,32 @@ import frc.robot.Constants;
 
 public class ShooterSubsystem extends SubsystemBase {
 
-  WPI_TalonFX shooterMotorMaster; //declare motors
+  WPI_TalonFX shooterMotorMaster; // declare motors
   WPI_TalonFX shooterMotorSlave;
 
   private double m_desiredSpeed;
 
   private Boolean isShooterUpToSpeed;
 
-
   /** Creates a new ShooterSubsystem. */
   public ShooterSubsystem() {
 
-  shooterMotorMaster = new WPI_TalonFX(Constants.CAN_ADDRESS_SHOOTER_MASTER); 
-  shooterMotorSlave = new WPI_TalonFX(Constants.CAN_ADDRESS_SHOOTER_SLAVE);
+    shooterMotorMaster = new WPI_TalonFX(Constants.CAN_ADDRESS_SHOOTER_MASTER);
+    shooterMotorSlave = new WPI_TalonFX(Constants.CAN_ADDRESS_SHOOTER_SLAVE);
 
-  initMasterMotorController(shooterMotorMaster);
-  initSlaveMotorController(shooterMotorSlave);
-  
-  shooterMotorSlave.setInverted(false); //invert slave (might change)
-  shooterMotorMaster.setInverted(true); //invert master (might change)
+    initMasterMotorController(shooterMotorMaster);
+    initSlaveMotorController(shooterMotorSlave);
 
-  shooterMotorSlave.follow(shooterMotorMaster);//
+    shooterMotorSlave.setInverted(false); // invert slave (might change)
+    shooterMotorMaster.setInverted(true); // invert master (might change)
 
+    shooterMotorSlave.follow(shooterMotorMaster);//
 
+    isShooterUpToSpeed = false;
 
-  isShooterUpToSpeed = false;
-
-  m_desiredSpeed = Constants.SHOOTER_DESIRED_VELOCITY;
+    m_desiredSpeed = Constants.SHOOTER_DESIRED_VELOCITY;
 
   }
-  
 
   @Override
   public void periodic() {
@@ -52,54 +48,54 @@ public class ShooterSubsystem extends SubsystemBase {
   }
 
   private static void initMasterMotorController(WPI_TalonFX falcon) {
-      
+
     System.out.println("Initializing Master: " + falcon);
-    
+
     // Config to Factory Default
     falcon.configFactoryDefault();
 
     falcon.setNeutralMode(NeutralMode.Coast); // Neutral Mode is "Coast"
-  
+
     falcon.setSensorPhase(true);
 
     falcon.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor, Constants.kPIDLoopIdx_Velocity,
         Constants.kTimeoutMs_Velocity);
-  
+
     falcon.configNominalOutputForward(0, Constants.kTimeoutMs_Velocity);
 
     falcon.configNominalOutputReverse(0, Constants.kTimeoutMs_Velocity);
-    
+
     falcon.configPeakOutputForward(1, Constants.kTimeoutMs_Velocity);
-    
+
     falcon.configPeakOutputReverse(1, Constants.kTimeoutMs_Velocity);
-  
+
   }
 
-   private static void initSlaveMotorController(WPI_TalonFX falcon) {
-    
+  private static void initSlaveMotorController(WPI_TalonFX falcon) {
+
     System.out.println("Initializing Slave: " + falcon);
-    
+
     // Config to Factory Default
     falcon.configFactoryDefault();
 
-     falcon.setNeutralMode(NeutralMode.Coast); // Neutral Mode is "Coast"
-     
-     falcon.setSensorPhase(true);
-     
-     falcon.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, Constants.kPIDLoopIdx_Velocity,
-         Constants.kTimeoutMs_Velocity);
-   
-     falcon.configNominalOutputForward(0, Constants.kTimeoutMs_Velocity);
+    falcon.setNeutralMode(NeutralMode.Coast); // Neutral Mode is "Coast"
 
-     falcon.configNominalOutputReverse(0, Constants.kTimeoutMs_Velocity);
-     
-     falcon.configPeakOutputForward(1, Constants.kTimeoutMs_Velocity);
-     
-     falcon.configPeakOutputReverse(-1, Constants.kTimeoutMs_Velocity);
-     
-   }
+    falcon.setSensorPhase(true);
 
-   private void configPIDFValues(double p, double i, double d, double f, int slot) {
+    falcon.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, Constants.kPIDLoopIdx_Velocity,
+        Constants.kTimeoutMs_Velocity);
+
+    falcon.configNominalOutputForward(0, Constants.kTimeoutMs_Velocity);
+
+    falcon.configNominalOutputReverse(0, Constants.kTimeoutMs_Velocity);
+
+    falcon.configPeakOutputForward(1, Constants.kTimeoutMs_Velocity);
+
+    falcon.configPeakOutputReverse(-1, Constants.kTimeoutMs_Velocity);
+
+  }
+
+  private void configPIDFValues(double p, double i, double d, double f, int slot) {
     // Configure the PID settings for Slot0
     shooterMotorMaster.config_kF(slot, f);
     shooterMotorMaster.config_kP(slot, p);
@@ -107,13 +103,19 @@ public class ShooterSubsystem extends SubsystemBase {
     shooterMotorMaster.config_kD(slot, d);
   }
 
-   public void setConstantVelocity() {
-    configPIDFValues(.1, 0, 0, 0.05, 0);  //STILL NEED TO GET THESE VALUES
-   
+  public void setConstantVelocity() {
+  public void setConstantVelocity(int velocity){  
+    configPIDFValues(.1, 0, 0, 0.05, 0); // STILL NEED TO GET THESE VALUES
     shooterMotorMaster.set(ControlMode.Velocity, m_desiredSpeed);
-
-    // m_desiredSpeed = 4000;
   }
+
+  public void setConstantVelocityFromInput(int velocity){  
+        //  Added these steps to allow us to set the constant velocity based on passed integer.
+        int m_velocity = velocity;
+        shooterMotorMaster.set(ControlMode.Velocity, m_velocity);
+  }
+
+
 
   // Stop our shooter to stop the shooter
   public void stopMotor() {
@@ -125,17 +127,17 @@ public class ShooterSubsystem extends SubsystemBase {
     return shooterMotorMaster.getSelectedSensorVelocity();
   }
 
-    //   Sets the desired speeed for our Shooter fly wheels
-  public void setDesiredSpeed(Double desiredSpeed){
+  // Sets the desired speeed for our Shooter fly wheels
+  public void setDesiredSpeed(Double desiredSpeed) {
     m_desiredSpeed = desiredSpeed;
   }
 
-  //   Gets the Desired Speed for the Shooter Fly Wheels
-  public double getDesiredSpeed(){
+  // Gets the Desired Speed for the Shooter Fly Wheels
+  public double getDesiredSpeed() {
     return m_desiredSpeed;
   }
 
-  public boolean isUpToSpeed(){
+  public boolean isUpToSpeed() {
     isShooterUpToSpeed = Math.abs(shooterMotorMaster.getSelectedSensorVelocity() - m_desiredSpeed) < 350;
     SmartDashboard.putBoolean("Is Shooter Up to Speed", isShooterUpToSpeed);
     System.out.println("Shooting Speed:  " + shooterMotorMaster.getSelectedSensorVelocity());
@@ -143,6 +145,4 @@ public class ShooterSubsystem extends SubsystemBase {
     return isShooterUpToSpeed;
   }
 
-  
-  }
-
+}
