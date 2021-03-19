f913bb08276025981a07a81d732f8c3d59c0eced
mbhoxsie <matt.hoxsie@gmail.com>
2021-03-19 19:15:26 -0400

    Cleaned up comments in the Hopper SS

--- src/main/java/frc/robot/subsystems/HopperSubsystem.java
+++ src/main/java/frc/robot/subsystems/HopperSubsystem.java
@@ -6,16 +6,14 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class HopperSubsystem extends SubsystemBase {
-  CANSparkMax myMotor;
+  CANSparkMax myMotor;  //Declare our Hopper motor as a SparkMax
 
   /** Creates a new ExampleSubsystem. */
   public HopperSubsystem() {
-    //myMotor = new CANSparkMax(10, MotorType.kBrushless);
     myMotor = new CANSparkMax(Constants.CAN_ADDRESS_HOPPER, MotorType.kBrushless);
   }
 
@@ -28,12 +26,18 @@ public class HopperSubsystem extends SubsystemBase {
   public void simulationPeriodic() {
     // This method will be called once per scheduler run during simulation
   }
+  //   Run the Hopper motor forward a inputed speed
   public void goForward(double speed){
     myMotor.set(speed);
   }
+
+  //Run the Hopper in reverse a inputed speed
+  //  NOTE:  Speed should be positive as we mulitply by (-1)
   public void goBackward(double speed){
     myMotor.set(speed*-1);
   }
+
+  //Stop the hopper motor
   public void stop(){
     myMotor.set(0);
   }
